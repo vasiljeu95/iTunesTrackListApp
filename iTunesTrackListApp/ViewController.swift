ec02ab4a79d8fb8cdade81bac4521c42234c9477
vasiljeu95 <vasilyeu95@gmail.com>
2020-03-08 21:57:03 +0300

    Initial Commit

---
+++ iTunesTrackListApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTunesTrackListApp
+//
+//  Created by Stepan Vasilyeu on 3/8/20.
+//  Copyright © 2020 Stepan Vasilyeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
